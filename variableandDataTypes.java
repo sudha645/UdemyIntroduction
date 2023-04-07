221fb885507ede731c8e5e9decef4910441bb08f
sudha645 <129913294+sudha645@users.noreply.github.com>
2023-04-06 22:06:39 -0400

    Add files via upload

---
+++ variableandDataTypes.java
@@ -0,0 +1,16 @@
+package CoreJavaBrushup;
+
+public class variableandDataTypes {
+
+	public static void main(String[] args) {
+		int myNum=8;
+		String website="RahulShetty";
+		char letter='c';
+		double num=30.5f;
+		boolean myCard=true;
+		System.out.println(myNum);
+		System.out.println(num);
+		System.out.println(website);
+		
+	}
+}

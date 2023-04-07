2e9ba4750866564cfbc476cb212b576b84399598
Sudha <tsudha610@gmail.com>
2023-04-06 21:59:49 -0400

    Arraya File

---
+++ Arrays.java
@@ -0,0 +1,23 @@
+package CoreJavaBrushup;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Arrays means storing a list of variables new is a keyword to create a memory
+		int arr[]=new int[5];
+
+		arr[0]=25;
+		arr[1]=30;
+		arr[2]=45;
+		arr[3]=47;
+		arr[4]=84;
+		
+		int arr2[]= {75,30,45,47,789,745,879,84};
+		//Forllop
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr2[i]);
+		}
+				}
+
+}
